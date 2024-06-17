04ec60de1af63c8e7170ac41007f68f9a93f5673
Tebogo Mahlalela <wisemanrsa@gmail.com>
2024-06-17 20:25:28 +0200

    Fixed project structure

---
+++ UmbracoHerosBlock.Core/Models/HeroBlockViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UmbracoHerosBlock.Core.UmbracoModels;
+
+namespace UmbracoHerosBlock.Core.Models
+{
+    public class HeroBlockViewModel
+    {
+        public HeroBlock? HeroBlock { get; set; }
+        public HeroCtaBlock? HeroCtaBlock { get; set; }
+    }
+}
